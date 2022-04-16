830f05ddff7e4afe2015fda8595e28ac3755fc14
dderevjanik <daniel.derevjanik@gmail.com>
2022-04-16 18:10:05 +0200

    Add colors, verbose and quiet options to parseAndGenerate

--- src/index.ts
+++ src/index.ts
@@ -8,11 +8,46 @@ export { generate } from "./generator";
 export { parseWsdl } from "./parser";
 
 export interface Options {
+    /**
+     * Generate only Definitions
+     * @default false
+     */
     emitDefinitionsOnly: boolean;
+    /**
+     * Prefix for generated interface names
+     * @default ""
+     */
     modelNamePreffix: string;
+    /**
+     * Suffix for generated interface names
+     * @default ""
+     */
     modelNameSuffix: string;
+    /**
+     * Case-insensitive name while parsing definition names
+     * @default false
+     */
     caseInsensitiveNames: boolean;
+    /**
+     * Maximum count of definition's with same name but increased suffix. Will throw an error if exceed
+     * @default 64 
+     */
     maxRecursiveDefinitionName: number;
+    /**
+     * Print verbose logs
+     * @default false
+     */
+    verbose: boolean;
+    /**
+     * Suppress all logs
+     * @default false
+     */
+    quiet: boolean;
+    /**
+     * Logs with colors
+     * @default true
+     */
+    colors: boolean;
 }
 
 export const defaultOptions: Options = {
@@ -20,7 +55,11 @@ export const defaultOptions: Options = {
     modelNamePreffix: "",
     modelNameSuffix: "",
     caseInsensitiveNames: false,
-    maxRecursiveDefinitionName: 64
+    maxRecursiveDefinitionName: 64,
+    //
+    verbose: false,
+    quiet: false,
+    colors: true
 };
 
 export async function parseAndGenerate(
@@ -32,6 +71,21 @@ export async function parseAndGenerate(
         ...defaultOptions,
         ...options,
     };
+
+    if (options.verbose) {
+        Logger.isDebug = true;
+    }
+    if (options.colors === false) {
+        Logger.colors = false;
+    }
+    if (options.quiet) {
+        Logger.isDebug = false;
+        Logger.isLog = false;
+        Logger.isInfo = false;
+        Logger.isWarn = false;
+        Logger.isError = false;
+    }
+
     // Logger.debug(`Options: ${JSON.stringify(mergedOptions, null, 2)}`);
 
     const timeParseStart = process.hrtime();
