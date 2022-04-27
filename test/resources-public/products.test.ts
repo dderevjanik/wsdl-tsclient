60d1eda3a1a5cacf5a42a7e4bb32259ccea0622f
dderevjanik <daniel.derevjanik@gmail.com>
2022-04-27 23:02:23 +0200

    Add more test

---
+++ test/resources-public/products.test.ts
@@ -0,0 +1,30 @@
+import test from "tape";
+import { existsSync } from "fs";
+import { parseAndGenerate } from "../../src";
+import { Logger } from "../../src/utils/logger";
+import { typecheck } from "../utils/tsc";
+
+const target = "products";
+
+test(target, async t => {
+    Logger.disabled();
+
+    const input = `./test/resources-public/${target}.wsdl`;
+    const outdir = "./test/generated";
+
+    t.test(`${target} - generate wsdl client`, async t => {
+        await parseAndGenerate(input, outdir);
+        t.end();
+    });
+
+    t.test(`${target} - check definitions`, async t => {
+        t.equal(existsSync(`${outdir}/foo/definitions/BankSvcRq.ts`), true);
+        t.end();
+    });
+
+    t.test(`${target} - compile`, async t => {
+        await typecheck(`${outdir}/file/index.ts`);
+		t.end();
+    });
+
+});
