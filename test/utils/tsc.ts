06fe824965330f870f8e1350762451031ca8d40b
Sander van Driel <sander.vandriel@gmail.com>
2024-05-14 07:47:29 +0200

    bugfix: make tsc invocation work on windows as well

--- test/utils/tsc.ts
+++ test/utils/tsc.ts
@@ -1,8 +1,10 @@
 import util from "util";
 
-const exec = util.promisify(require('child_process').exec);
-const pathToTsc = `./node_modules/.bin/tsc`;
+const exec = util.promisify(require("child_process").exec);
 
 export async function typecheck(pathToIndex: string) {
-    await exec(`${pathToTsc} ${pathToIndex} --noEmit`);
+    const cmd = `tsc ${pathToIndex} --noEmit`;
+    await exec(cmd, {
+        env: process.env,
+    });
 }
